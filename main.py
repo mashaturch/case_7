6644ed6a4aa8b64835bc76e2c5444256c31e2992
mashaturch <73123894+mashaturch@users.noreply.github.com>
2021-04-10 23:45:44 +0700

    Update main.py
    start

---
+++ main.py
@@ -0,0 +1,23 @@
+"""Case-study #7 Text generator
+Разработчики:
+Турчинович М. (), Зубарева Т. () , Костылев М. ()
+"""
+
+def delete_characters():
+    """Removal of service characters"""
+    with open ('input.txt', encoding = 'utf-8') as f_in:
+        input_text = f_in.readlines()
+    text = ''
+    punctuation_symbols = list('"#$%&\'()*+-/:;<=>@[\\]^_`{|}~—')
+    print(punctuation_symbols)
+    incorrect_basic_symbols = [' .', ' !', ' ?', ' ,']
+    basic_symbols = ['.', '!', '?', ',']
+    for string in input_text:
+        text += string[:len(string) - 1] + ' '
+    text += input_text[-1][-1]
+    for char in punctuation_symbols:
+        text = text.replace(char, '')
+    text = text.replace('  ', '')
+    for i in range(len(incorrect_basic_symbols)):
+        text = text.replace(incorrect_basic_symbols[i], basic_symbols[i])
+    return text
