159e97c3b73c30dd3fbf1770b5f807efe4300bc7
mashaturch <s1yguvzg>
2021-04-11 23:19:10 +0700

    end

--- main.py
+++ main.py
@@ -3,12 +3,10 @@
 Турчинович М. (), Зубарева Т. () , Костылев М. ()
 """
 import numpy as np
-import random
-from collections import defaultdict
 
-def delete_characters():
+def delete_characters(name_file):
     """Removal of service characters"""
-    with open ('input.txt', encoding = 'utf-8') as f_in:
+    with open(name_file, encoding='utf-8') as f_in:
         input_text = f_in.readlines()
     text = ''
     punctuation_symbols = list('"#$%&\()*+-/:;<=>@[]^_`{|}~—–«»')
@@ -19,50 +17,55 @@ def delete_characters():
     text += input_text[-1][-1]
     for char in punctuation_symbols:
         text = text.replace(char, '')
-    text = text.replace('  ', '')
     for i in range(len(incorrect_basic_symbols)):
         text = text.replace(incorrect_basic_symbols[i], basic_symbols[i])
+    text = text.replace('  ', ' ')
     return text
 
-
-ind_words = delete_characters().split()
 def make_pairs(ind_words):
+    """Function for concatenating pairs of words"""
     for i in range(len(ind_words) - 1):
         yield (ind_words[i], ind_words[i + 1])
-pair = make_pairs(ind_words)
-word_dict = {}
 
+# entered data
+name_file = input('Имя файла: ')
+number_of_offers = int(input('Количество генерируемых предложений: '))
+
+
+ind_words = delete_characters(name_file).split()
+pair = make_pairs(ind_words)
+
+# sorting similar words
+word_dict = {}
 for word_1, word_2 in pair:
     if word_1 in word_dict.keys():
         word_dict[word_1].append(word_2)
     else:
         word_dict[word_1] = [word_2]
 
-
+# compiling a list of starting words
 start_words = []
-for ch in word_dict:
-    if ch == ch.capitalize():
-        if not ch in start_words:
-            start_words.append(ch)
+for word in word_dict:
+    if word == word.capitalize():
+        if not (word in start_words):
+            start_words.append(word)
 
-print(start_words)
 end_symbols = ['.', '!', '?']
 
-last_words = []
-for sh in word_dict:
-    if sh[-1] in end_symbols:
-        if not (sh in last_words):
-            last_words.append(sh)
-print(last_words)
-
-
-first_word = np.random.choice(start_words)
-while first_word.istitle():
+# random compilation of new text
+k = 0
+while k < number_of_offers:
+    first_word = np.random.choice(start_words)
     chain = [first_word]
     n_words = 20
-    first_word = np.random.choice(start_words)
-
     for i in range(n_words):
         chain.append(np.random.choice(word_dict[chain[-1]]))
-
-    print(' '.join(chain))
+        if chain[-1][-1] in end_symbols:
+            break
+    print(' '.join(chain), end='')
+    k += 1
+    if chain[-1][-1] != '.':
+        print('.', end=' ')
+        k += 1
+    else:
+        print(' ', end='')
